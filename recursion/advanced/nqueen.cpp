b97494c39fe9c26a92320d466c19af55a6ebaaf4
Mohit Tiwari <mt0988145@gmail.com>
2026-01-24 23:29:28 +0530

    adding files

---
+++ recursion/advanced/nqueen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int size =4;
+
+bool vaild(int k ,int l ){
+        for(int i =1;i<size;i++)
+        {
+            
+        }
+
+
+}
+
+int main() {
+    
+    return 0;
+}
