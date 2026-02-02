cefc1df4e512081a0fed5c0303ddfd9cb6bcd098
Mohit Tiwari <mt0988145@gmail.com>
2026-02-02 19:35:51 +0530

    adding files

---
+++ LinkedList/dynamicInput.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+};
+// function for creating Node at runtime
+// it will return head of the Linked List
+Node* inputFromTail(){
+    cout<<"give the List Item\n";
+    
+    int data ;
+    cin>>data;
+    // creating head and tail
+    Node *head=NULL ,*tail=NULL;
+    while(data!=-1){
+        Node *n = new Node(data);
+        if(head==NULL) {
+            head = n;
+            tail =n;
+        }else{
+          
+            tail->next =n;
+            tail=n;
+        }
+        cin>>data;
+    }
+    return head;
+}
+Node* inputFromHead(){
+    cout<<"give the List Item\n";
+    
+    int data ;
+    cin>>data;
+    // creating head and tail
+    Node *head=NULL ,*tail=NULL;
+    while(data!=-1){
+        Node *n = new Node(data);
+        if(head==NULL) {
+            head = n;
+            tail =n;
+        }else{
+          
+           n->next=head;
+           head=n;
+        }
+        cin>>data;
+    }
+    return head;
+}
+// printLinkedList
+void print(Node *head)
+{
+    Node *temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<"End of LL\n";
+}
+
+int main() {
+Node *head1 =inputFromTail();
+print(head1);
+Node *head2 =inputFromHead();
+print(head2);
+    return 0;
+}
