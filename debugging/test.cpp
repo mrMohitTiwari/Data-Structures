0ab4a818f62a126099945add8862155b44ae50e5
Mohit Tiwari <mt0988145@gmail.com>
2026-02-09 23:26:31 +0530

    updating

---
+++ debugging/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =4;
+    cout<<a;
+    int arr[4]={1,2,3,3};
+    for(int i =0;i<4;i++)
+    cin>>arr[i];
+    
+    return 0;
+}
