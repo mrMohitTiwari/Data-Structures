a1c2ffc1407bfb4dd0405b45363614cbc74a3229
Mohit Tiwari <mt0988145@gmail.com>
2026-02-24 22:26:43 +0530

    adding files

--- recursion/Beignner/multiply.cpp
+++ recursion/Beignner/multiply.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
-int multiply(int n ,int k)
+// #include <iostream>
+// using namespace std;
+// int multiply(int n ,int k)
 
-{
-    if(k==1)
-    return n;
-    return n+multiply(n,k-1);
-}
-int main() {
+// {
+//     if(k==1)
+//     return n;
+//     return n+multiply(n,k-1);
+// }
+// int main() {
     
-    cout<<multiply(2,3); 
-    return 0;
-}
+//     cout<<multiply(2,3); 
+//     return 0;
+// }
