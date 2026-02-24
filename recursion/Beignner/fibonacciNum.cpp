97c963e3b86aebd214e7c007359272609b1f4859
Mohit Tiwari <mt0988145@gmail.com>
2026-02-24 22:26:20 +0530

    adding files

--- recursion/Beignner/fibonacciNum.cpp
+++ recursion/Beignner/fibonacciNum.cpp
@@ -1,15 +1,15 @@
-// #include <iostream>
-// using namespace std;
-// int fib(int n )
-// {
-//     if(n==0 || n==1) return n;
-//     return fib(n-1) + fib(n-2);
-// }
+#include <iostream>
+using namespace std;
+int fib(int n )
+{
+    if(n==0 || n==1) return n;
+    return fib(n-1) + fib(n-2);
+}
 
-// int main() {
-//     int n ;
-//     cin>>n;
+int main() {
+    int n ;
+    cin>>n;
     
-//     cout<<fib(n)<<endl;
-//     return 0;
-// }
+    cout<<fib(n)<<endl;
+    return 0;
+}
