b81b9d2860f0f83f3bee9627c69144d7db44a1a7
Mohit Tiwari <mt0988145@gmail.com>
2026-03-28 22:07:40 +0530

    adding files

---
+++ Stack/validParanthesis.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValidParenthesis(string st)
+{
+    stack<char> s;
+
+    for (int i = 0; i < st.size(); i++)
+    {
+        if (st[i] == '(' || st[i] == '{' || st[i] == '[')
+        {
+            s.push(st[i]);
+        }
+        else
+        {
+            if (s.size() == 0)
+                return false;
+
+            if ((s.top() == '(' && st[i] == ')') ||
+                (s.top() == '[' && st[i] == ']') ||
+                (s.top() == '{' && st[i] == '}'))
+            {
+                s.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    return s.size() == 0;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << isValidParenthesis(s);
+    return 0;
+}
