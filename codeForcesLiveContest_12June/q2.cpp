ef9ea350a192851188d5d46fe6c24e6b1743e385
Mohit Tiwari <mt0988145@gmail.com>
2026-06-14 08:43:13 +0530

    adding files

--- codeForcesLiveContest_12June/q2.cpp
+++ codeForcesLiveContest_12June/q2.cpp
@@ -13,35 +13,33 @@ int main() {
         cin>>s;
         bool flip =0;
         int i =0;
-        for(int i =0;i<n;i++){
-                if(s[i]=='0') break;
+        for(;i<n;i++){
+          
+                if(s[i]=='1') break;
 
         }
-        if(i==n) cout<<"YES\n";
-        else{
-            if(i+k>=n) {cout<<"NO\n";
-            continue;}
-            else {
-                if(s[i]=='0'){
-                    s[i]='1';
-                
-                }
-                else s[i] = '0';
-                if(s[i+k]=='0') s[i+k]='1';
-                else s[i+k] = '0';
-            }
-        }
-        // checking if string is 0 now
-            i =0;
-            for(;i<n;i++)
-            {
-                if(s[i]=='1') {
-                    cout<<"NO\n";
-                    break;
-                    
-                }
-            }
-            if(i==n) cout<<"YES\n";
+     if(i==n){
+         
+        cout<<"yes\n";
+        continue;
+     }else if(i+k>=n || s[i]=='0'||s[i+k]=='0') {
+        cout<<"NO\n";
+        continue;
+     }
+     s[i] = '0';
+     s[i+k] = '0' ;
+
+     i = 0; 
+     for(;i<n;i++)
+     {
+        if(s[i]=='1') break;
+     }
+     if(i==n)  cout<<"YES\n";
+     else cout<<"NO\n";
+
+
+
+        
     }
     return 0;
 }
---
+++ codeForcesLiveContest_12June/q3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, x;
+        cin >> a >> b >> x;
+        if (a == b)
+        {
+            cout << 0 << "\n";
+            continue;
+        }
+        int opr = 0;
+        // if(a && a<x && b<x &&b){
+        //     cout<<2<<'\n';
+        //     continue;
+        // }
+        while (a != b)
+        {
+
+            int t_d1, t_d2;
+            if (b > a)
+            {
+                int t = a;
+                a = b;
+                b = t;
+            }
+                // incrememting b 
+                if(!a ||!b) {
+                    ++opr;
+                    break;
+                }
+                // incrementing b 
+                ++b;
+                int d1 = a-b;
+                if(d1==0) {
+                    ++opr;
+                    break;
+                }
+                --b;
+                // dividing by x
+                 int t_a = a;
+                 
+                 a = a/x;
+                 int d2 = a-b;
+                 if(d2==0) {
+                    ++opr;
+                    break;
+                }
+                 if(d2<0||a==0){
+                        opr+=2;
+                            break;
+                 }
+                 if(d1<d2){
+                        a = t_a;
+                        ++opr;
+                 }
+                 else {
+                    ++opr;
+                 }
+
+
+
+           
+        }
+        cout << opr << "\n";
+    }
+    return 0;
+}
